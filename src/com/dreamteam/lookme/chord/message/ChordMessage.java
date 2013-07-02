14c67094f04f4a3f5636b86b8fd417f146a779db
carlo_tassi <carlo.tassi984@gmail.com>
2013-07-02 20:57:41 +0200

    Bozza dei messaggi scambiati over chord dai device

---
+++ src/com/dreamteam/lookme/chord/message/ChordMessage.java
@@ -0,0 +1,106 @@
+/*
+ * Author: Carlo Tassi
+ * 
+ * Message object over chord for Look@me App
+ */
+package com.dreamteam.lookme.chord.message;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChordMessage implements Serializable {
+
+	private static final long serialVersionUID = -2069620040307561844L;
+	
+	private final MessageType mType;			/* Message type 		*/
+	private final Map<String, Object> mPayload;	/* Data to transfer		*/
+	private String mSenderNodeName;				/* Name of sender node 	*/
+	
+	private ChordMessage(MessageType type) {
+		this.mType = type;
+		mPayload = new HashMap<String, Object>();
+	}
+	
+	public static ChordMessage obtainMessage(MessageType type) {
+		return new ChordMessage(type);
+	}
+
+	public void putInt(String key, int value) {
+		mPayload.put(key, value);
+	}
+
+	public void putString(String key, String value) {
+		mPayload.put(key, value);
+	}
+
+	public void putObject(String key, Object value) {
+		mPayload.put(key, value);
+	}
+
+	public int getInt(String key) {
+		return (Integer) mPayload.get(key);
+	}
+
+	public String getString(String key) {
+		return (String) mPayload.get(key);
+	}
+
+	public Object getObject(String key) {
+		return mPayload.get(key);
+	}
+
+	public MessageType getType() {
+		return mType;
+	}
+	
+	public String getSenderNodeName() {
+		return this.mSenderNodeName;
+	}
+
+	public void setSenderNodeName(String senderNodeName) {
+		mSenderNodeName = senderNodeName;
+	}
+	
+	/*
+	 * Return message object as byte array
+	 */
+	byte[] getBytes() {
+		final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		final ObjectOutputStream os;
+
+		try {
+			os = new ObjectOutputStream(out);
+			os.writeObject(this);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return out.toByteArray();
+	}
+	
+	/*
+	 * Recreates message object from the byte array
+	 */
+	public static ChordMessage obtainChordMessage(byte[] data, String senderNodeName) {
+		final ByteArrayInputStream in = new ByteArrayInputStream(data);
+		final ObjectInputStream is;
+		ChordMessage message = null;
+
+		try {
+			is = new ObjectInputStream(in);
+			message = (ChordMessage) is.readObject();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		message.mSenderNodeName = senderNodeName;
+		return message;
+	}
+
+}
---
+++ src/com/dreamteam/lookme/chord/message/MessageType.java
@@ -0,0 +1,22 @@
+/*
+ * Author: Carlo Tassi
+ * 
+ * Enumeration of all message types that can be 
+ * sent over chord by Look@me App
+ */
+package com.dreamteam.lookme.chord.message;
+
+public enum MessageType {
+	
+	PREVIEW, 	/* Minimal profile data  */
+	PROFILE, 	/* Complete profile data */
+	INTEREST, 	/* Interest data 		 */
+	SHARED,		/* Shared files data 	 */
+	LIKE;		/* Love game			 */ 
+	
+	@Override
+	public String toString() {
+		return name();
+	}
+
+}
