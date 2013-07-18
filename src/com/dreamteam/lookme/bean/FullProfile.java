a137415af37a65333efd73c80d189163e3263ac7
carlo_tassi <carlo.tassi984@gmail.com>
2013-07-18 18:42:56 +0200

    classi bean

---
+++ src/com/dreamteam/lookme/bean/FullProfile.java
@@ -0,0 +1,37 @@
+package com.dreamteam.lookme.bean;
+
+public class FullProfile extends BasicProfile {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private String surname;
+	private byte[][] profileImages;
+	private String status;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public byte[][] getProfileImages() {
+		return profileImages;
+	}
+	public void setProfileImages(byte[][] profileImages) {
+		this.profileImages = profileImages;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
