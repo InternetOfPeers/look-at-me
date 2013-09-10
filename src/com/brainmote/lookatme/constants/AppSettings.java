26a4d39f0d073246be6d25daf5b30a2ed10b1206
Giuseppe Bertone <service@brainmote.com>
2013-09-10 12:26:14 +0200

    Ripristinata la gestione della ricreazione delle activity per quelle activity che non aveva dato problemi in precedenza. Non so perché ma ho l'impressione che impedire ad Android di gestire tutte le activity a tappeto, non faccia bene alla gestione della memoria dell'app. Allungato il tempo di attesa dell'immagine di profilo, perché ho visto che a volte ci mette più di 10 secondi. Sarebbe da capire come mai, visto che ho conrollato la dimensioni dell'immagine incriminata non arrivavamo a 100Kb, mentre un'altra da 600Kb la caricava velocemente senza problemi;

--- src/com/brainmote/lookatme/constants/AppSettings.java
+++ src/com/brainmote/lookatme/constants/AppSettings.java
@@ -5,7 +5,7 @@ public interface AppSettings {
 	// 3500 è la durata di default di un Toast.LENGTH_LONG
 
 	static final long SPLASH_DISPLAY_LENGHT = 3000; // Millisecondi
-	static final long LOADING_PROFILE_TIMEOUT = 10000; // Millisecondi
+	static final long LOADING_PROFILE_TIMEOUT = 20000; // Millisecondi
 	static final long WAIT_BEFORE_SHOWING_LOADING_PROFILE_DIALOG = 500; // Millisecondi
 	static final long DOUBLE_BACK_TO_REALLY_EXIT_DELAY = 2000; // Millisecondi
 
