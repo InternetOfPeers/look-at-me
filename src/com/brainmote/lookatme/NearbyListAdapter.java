019fa7d5ffe045110a2ef522c0022c6050da6fe7
alfaomega83 <riccardoalfrilli@gmail.com>
2013-09-24 22:15:13 +0200

    rimosso thread per caricamento immagini,caricava in maniera randomica solo porzioni di esse

--- src/com/brainmote/lookatme/NearbyListAdapter.java
+++ src/com/brainmote/lookatme/NearbyListAdapter.java
@@ -7,23 +7,25 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.brainmote.lookatme.bean.BasicProfile;
 import com.brainmote.lookatme.chord.Node;
 import com.brainmote.lookatme.service.Services;
 import com.brainmote.lookatme.util.ImageUtil;
-import com.brainmote.lookatme.util.Log;
 
 public class NearbyListAdapter extends BaseAdapter {
 
 	private Activity activity;
 
 	public NearbyListAdapter(Activity activity) {
+		super();
 		this.activity = activity;
 	}
 
@@ -53,16 +55,16 @@ public class NearbyListAdapter extends BaseAdapter {
 		}
 		// Imposto l'immagine del profilo
 		ImageView photoImage = (ImageView) convertView.findViewById(R.id.profilePhotoImage);
-		// ProgressBar loadingImage = (ProgressBar)
-		// convertView.findViewById(R.id.loadingImageProgressBar);
+		photoImage.setTag(position);
+//		 ProgressBar loadingImage = (ProgressBar)
+//		 convertView.findViewById(R.id.loadingImageProgressBar);
 		BasicProfile profile = (BasicProfile) getItem(position).getProfile();
-		ImageLoader imageLoader = new ImageLoader(photoImage, profile, activity);
-		imageLoader.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
-		// Bitmap mainImageProfile =
-		// ImageUtil.getBitmapProfileImage(activity.getResources(), profile);
-		// Bitmap croppedImageProfile =
-		// ImageUtil.bitmapForThumbnail(mainImageProfile);
-		// photoImage.setImageBitmap(croppedImageProfile);
+//		ImageLoader imageLoader = new ImageLoader(photoImage,loadingImage, position,profile, activity);
+//		imageLoader.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
+		
+		Bitmap mainImageProfile = ImageUtil.getBitmapProfileImage(activity.getResources(), profile);
+		Bitmap croppedImageProfile = ImageUtil.bitmapForThumbnail(mainImageProfile);
+		photoImage.setImageBitmap(croppedImageProfile);
 		// Imposto i liked
 		ImageView likedImage = (ImageView) convertView.findViewById(R.id.imageLiked);
 		if (Services.currentState.getLikedSet().contains(getItem(position).getProfile().getId())) {
@@ -73,41 +75,51 @@ public class NearbyListAdapter extends BaseAdapter {
 		}
 		return convertView;
 	}
-
+	
 	public class ImageLoader extends AsyncTask<String, Integer, Bitmap> {
 
-		private final WeakReference<ImageView> imageViewReference;
-		// private final WeakReference<ProgressBar> loadingImageReference;
-
+//		private final WeakReference<ImageView> imageViewReference;
+//		 private final WeakReference<ProgressBar> loadingImageReference;
+		 
+			private final ImageView imageViewReference;
+			 private final ProgressBar loadingImageReference;
+			 private final int position;	
+		 
 		private final BasicProfile profile;
 
-		public ImageLoader(ImageView imageView, BasicProfile profile, Context context) {
-			imageViewReference = new WeakReference<ImageView>(imageView);
-			// loadingImageReference = new WeakReference<ProgressBar>(
-			// loadingImage );
+		public ImageLoader(ImageView imageView,ProgressBar loadingImage,int position, BasicProfile profile, Context context) {
+			imageViewReference = (imageView);
+			 loadingImageReference = (loadingImage );
+			 this.position=position;
 			this.profile = profile;
 
 		}
 
 		@Override
 		protected Bitmap doInBackground(String... params) {
+			Bitmap returnBitmap=null;
 			try {
+				
 				Bitmap mainImageProfile = ImageUtil.getBitmapProfileImage(activity.getResources(), profile);
-				return ImageUtil.bitmapForThumbnail(mainImageProfile);
+				returnBitmap= ImageUtil.bitmapForThumbnail(mainImageProfile);
+				
 			} catch (Exception e) {
-				Log.e(e.toString());
-				return null;
+				Log.e("async loading class",e.toString());
 			}
+			return returnBitmap;
 		}
 
 		@Override
 		protected void onPostExecute(Bitmap bitmap) {
-			ImageView imageView = imageViewReference.get();
-			if (imageView != null) {
-				imageView.setImageBitmap(bitmap);
+			ImageView imageView = imageViewReference;
+			if (imageView != null&&((Integer)imageView.getTag()==this.position)) {
+					imageView.setImageBitmap(bitmap);	
+					imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+				
+				imageViewReference.setVisibility(View.VISIBLE);
+				loadingImageReference.setVisibility(View.INVISIBLE);
 			}
-			// imageViewReference.get().setVisibility(View.VISIBLE);
-			// loadingImageReference.get().setVisibility(View.INVISIBLE);
+
 
 		}
 
