db474a71749be89e89f0670ebe4d1dd6e9d62757
carlo_tassi <carlo.tassi984@gmail.com>
2013-10-10 13:31:52 +0200

    esempio di contatti

--- src/com/brainmote/lookatme/ContactListAdapter.java
+++ src/com/brainmote/lookatme/ContactListAdapter.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.QuickContactBadge;
 
 public class ContactListAdapter extends BaseAdapter {
@@ -48,10 +51,31 @@ public class ContactListAdapter extends BaseAdapter {
 			convertView = layoutInflater.inflate(R.layout.contact_list_row, null);
 		}
 		QuickContactBadge contactBadge = (QuickContactBadge) convertView.findViewById(R.id.contactBadge);
-		contactBadge.setImageResource(R.drawable.canada);
+		contactBadge.setImageResource(R.drawable.ic_profile_image);
 		contactBadge.assignContactFromEmail("any@gmail.com", true);  
 		contactBadge.assignContactFromPhone("3211234567", true);
 		contactBadge.setMode(ContactsContract.QuickContact.MODE_SMALL); 
+		
+		ImageView imageFacebook = (ImageView) convertView.findViewById(R.id.imageContactFacebook);
+		imageFacebook.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://www.facebook.com"));
+		        activity.startActivity(intent);
+		    }
+		});
+		ImageView imageLinkedin = (ImageView) convertView.findViewById(R.id.imageContactLinkedin);
+		imageLinkedin.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://www.linkedin.com"));
+		        activity.startActivity(intent);
+		    }
+		});
 		return convertView;
 	}
 
